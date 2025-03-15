4e865576a0c34b28ce4895905761d9d98d668335
Ryan Ratajczak <ryantratajczak@gmail.com>
2025-03-14 18:34:30 -0600

    update outdated brand name
    on July 23rd, 2023. The website formally known as "Twitter"
    **officially** rebranded to 'X'.
    This change reflects the new branding and will no longer leave users
    confused.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,17 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 w-full flex items-center justify-center gap-4 p-4 border-t z-10 bg-background">
       <span>© {new Date().getFullYear()} Robert Shirts</span>
-      <Link href="https://github.com/robertjshirts/place" className="hover:underline">
+      <Link
+        href="https://github.com/robertjshirts/place"
+        className="hover:underline"
+      >
         GitHub
       </Link>
       <Link href="https://x.com/callanjerel" className="hover:underline">
-        Twitter
+        X
       </Link>
     </footer>
   );
